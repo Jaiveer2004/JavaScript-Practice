c28b77ca11aa781c13949f41b4b9e1c7cbbf05b0
Jaiveer Singh <157090396+Jaiveer2004@users.noreply.github.com>
2024-10-16 15:37:33 +0000

    Added

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,35 @@
+const name = "Jaiveer"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value") // Outdated
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`)
+
+const gameName = new String("Jaiveer-Singh")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("e")) // First Occ
+
+const newString = gameName.substring(0, 4)
+// console.log(newString)
+
+const anotherString = gameName.slice(1, 4)
+// console.log(anotherString)
+
+const newStringOne = "   Jaiveer    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim()) // removes spaces from start and end
+
+const url = "https://www.google.com/"
+
+// console.log(url.replace("www.", ""))
+// console.log(url.includes("www"))
+
+// Convert String to Array based on a condition
+// console.log(url.split("."))
+

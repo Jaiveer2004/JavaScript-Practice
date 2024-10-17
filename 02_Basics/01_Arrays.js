fea7971b8b034f473bd57282fa943d1f7cf9ecc1
Jaiveer Singh <157090396+Jaiveer2004@users.noreply.github.com>
2024-10-17 16:13:02 +0000

    Added

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// const myArray = [0, 1, 2, 3, 4, 5, true, "Jaiveer"]
+// Arrays are resizable.
+
+// console.log(myArr[0]);
+
+// Difference between Slice and Splice
+// Splice changes original array
+// Slice changes copy of array
